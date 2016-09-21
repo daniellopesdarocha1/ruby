1beb880eb81a9f9ce6fdb4e35f731b5fd8f44551
daniellopesdarocha1 <daniellopesdarocha1@gmail.com>
2016-09-21 15:18:54 -0300

    linhas, comentários e ifs

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,29 +1,30 @@
-puts "Bem vindo ao jogo da adivinhação"
-puts
-
+puts "\nBem vindo ao jogo da adivinhação\n"
 puts "Qual é o seu nome?"
 nome = gets
 
-puts
-puts
-
+puts "\n\n\n"
 puts "Começaremos o jogo para você, " + nome
-
 puts "Escolhendo um número secreto entre 1 e 200..."
 numero_secreto = 175
+puts "Escolhido... Adivinhe o numero"
 
-puts "Escolhido?... Adivinhe o numero"
-
-puts
-puts
-
+puts "\n\n\n\n"
 puts "Tentativa 1"
 puts "Entre com o numero:"
 chute = gets 
-
 puts "Será que acertou? Você chutou " + chute
 
-puts chute.to_i == numero_secreto
-#puts "175" == 175 #false
-#puts "175" == "175" #true
-#puts "175".to_i == 175 #true
+acertou = chute.to_i == numero_secreto
+
+if acertou
+	puts "Acertou!"
+else
+	maior = numero_secreto > chute.to_i
+	if maior
+		puts "O número secreto é maior!"
+	else
+		puts "O número secreto é menor!"
+	end
+end
+
+
--- testes.rb
+++ testes.rb
@@ -1,3 +1,11 @@
-puts 175 == 175 
-puts 174 == 175 
-puts "175" == 175 
+puts 175 == 175 #true
+puts 174 == 175 #false
+puts "175" == 175 #false
+puts "175" == "175" #true
+puts "175".to_i == 175 #true
+
+=begin
+	
+isto é um comentário de várias linhas
+	
+end
