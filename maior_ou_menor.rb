6c6a64311eee5765243001dba82b50451a05fa92
Daniel Lopes da Rocha <daniellopesdarocha1@gmail.com>
2016-10-03 16:27:56 -0300

    Utilizando ascii art

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,16 +1,63 @@
 def da_boas_vindas
-	puts "Bem vindo ao jogo da adivinhacao"
-	puts "Qual e o seu nome?"
-	nome = gets.strip #strip apaga os espaços em branco da string
-
-	puts "\n\n\n"
-	puts "Comecaremos o jogo para voce, " + nome
+    puts
+    puts "        P  /_\  P                              "
+    puts "       /_\_|_|_/_\                             "
+    puts "   n_n | ||. .|| | n_n         Bem vindo ao    "
+    puts "   |_|_|nnnn nnnn|_|_|     Jogo de Adivinhacao!"
+    puts "  |' '  |  |_|  |'  ' |                        "
+    puts "  |_____| ' _ ' |_____|                        "
+    puts "        \__|_|__/                              "
+    puts
+    puts "Qual eh o seu nome?"
+    nome = gets.strip
+    puts "\n\n\n\n\n\n"
+    puts "Comecaremos o jogo para voce, #{nome}"
+    nome
 end
 
-def sorteia_numero_secreto
-	puts "Escolhendo um numero secreto entre 1 e 200..."
-	sorteado = rand(200)
-	puts "Escolhido... Adivinhe o numero"
+def ganhou
+    puts
+    puts "             OOOOOOOOOOO               "
+    puts "         OOOOOOOOOOOOOOOOOOO           "
+    puts "      OOOOOO  OOOOOOOOO  OOOOOO        "
+    puts "    OOOOOO      OOOOO      OOOOOO      "
+    puts "  OOOOOOOO  #   OOOOO  #   OOOOOOOO    "
+    puts " OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   "
+    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+    puts "OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  "
+    puts " OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   "
+    puts "  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    "
+    puts "    OOOOO   OOOOOOOOOOOOOOO   OOOO     "
+    puts "      OOOOOO   OOOOOOOOO   OOOOOO      "
+    puts "         OOOOOO         OOOOOO         "
+    puts "             OOOOOOOOOOOO              "
+    puts
+    puts "               Acertou!                "
+    puts
+end
+
+def pede_dificuldade
+	puts "Qual a dificuldade que deseja? (1 facil, 5 dificil)"
+	dificuldade = gets.to_i
+end
+
+def sorteia_numero_secreto (dificuldade)
+	case dificuldade
+	when 1
+		maximo = 30
+	when 2
+		maximo = 60
+	when 3
+		maximo = 100
+	when 4
+		maximo = 150
+	else
+		maximo = 200
+	end
+	puts "Escolhendo um numero secreto entre 1 e #{maximo}..."
+	sorteado = rand(maximo) +1
+	puts "Escolhido... Adivinhe o numero."
 	#return sorteado
 	sorteado # a ultima instrução de uma função é o que será retornado
 end
@@ -29,7 +76,7 @@ def verifica_se_acertou (numero_secreto, chute)
 	acertou = chute == numero_secreto
 
 	if acertou
-		puts "Acertou!"
+		ganhou
 		return true
 	end
 
@@ -42,27 +89,48 @@ def verifica_se_acertou (numero_secreto, chute)
 	false #retorno da função
 end
 
+def joga (nome, dificuldade)
+	pontos_ate_agora = 1000
+	limite_de_tentativas = 5
+	chutes = []
 
-da_boas_vindas
-numero_secreto = sorteia_numero_secreto
+	for tentativa in 1..limite_de_tentativas
 
-pontos_ate_agora = 1000
-limite_de_tentativas = 5
-chutes = []
+		chute = pede_um_numero chutes, tentativa, limite_de_tentativas
+		chutes << chute
 
-for tentativa in 1..limite_de_tentativas
+		if nome == "Daniel"
+			ganhou
 
-	chute = pede_um_numero chutes, tentativa, limite_de_tentativas
-	chutes << chute
-	pontos_a_perder = (chute - numero_secreto).abs / 2.0 #para retornar float deve se dividir com a casa decimal.
+			break
+		end
 
-	pontos_ate_agora -= pontos_a_perder
+		pontos_a_perder = (chute - numero_secreto).abs / 2.0 #para retornar float deve se dividir com a casa decimal.
+		pontos_ate_agora -= pontos_a_perder
 
-	if verifica_se_acertou numero_secreto, chute
+		if verifica_se_acertou numero_secreto, chute
+			break
+		end
+	end
+
+	puts "Voce ganhou #{pontos_ate_agora} pontos."
+
+	#unless é um if de dupla negação
+end
+
+def nao_quer_jogar?
+	puts "Deseja jogar novamente? (S/N)"
+	quero_jogar = gets.strip
+	nao_quero_jogar = quero_jogar.upcase == "N"
+end
+
+nome = da_boas_vindas
+dificuldade = pede_dificuldade
+numero_secreto = sorteia_numero_secreto dificuldade
+
+loop do
+	joga nome, dificuldade
+	if nao_quer_jogar?
 		break
 	end
 end
-
-puts "Voce ganhou #{pontos_ate_agora} pontos."
-
-#unless é um if de dupla negação
